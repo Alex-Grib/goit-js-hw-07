5c5a70fa54dbdb64529a910add7f07951c7e8761
Alexandr Grib <alexgribaldi@gmail.com>
2020-11-15 15:10:53 +0100

    Устраняет ошибки

--- js/task-5.js
+++ js/task-5.js
@@ -1,12 +1,15 @@
-const nameInputEl = document.querySelector('#name-input');
-const nameOutputEl = document.querySelector('#name-output');
 
-nameInputEl.addEventListener('input', onNameInputElSubmit);
-function onNameInputElSubmit(event) {
-  const name = nameInputEl.value;
-  if (name === '') {
-    nameOutputEl.textContent = 'незнакомец';
-  } else {
-    nameOutputEl.textContent = name;
-  }
-}
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+
+
+
+
+const inputRef = document.querySelector('#name-input');
+const nameOutRef = document.querySelector('#name-output');
+
+inputRef.addEventListener('input', event => {
+  nameOutRef.textContent =
+    event.target.value === '' ? 'незнакомец' : event.target.value;
+});
+// console.log(nameOutRef.textContent);
--- js/task-6.js
+++ js/task-6.js
@@ -1,17 +1,26 @@
-const validationInput = document.querySelector("#validation-input");
 
-const validationInputLength = function () {
-  if (
-    validationInput.value.length < Number(validationInput.dataset.length) ||
-    validationInput.value.length > Number(validationInput.dataset.length)
-  ) {
-    validationInput.classList.add("invalid");
-  } else {
-    validationInput.classList.add("valid");
-  }
-};
 
-validationInput.addEventListener("focus", () => {
-  validationInput.classList = "";
-});
-validationInput.addEventListener("blur", validationInputLength);
+/* Taks 6
+Напиши скрипт, который бы при потере фокуса на инпуте, 
+проверял его содержимое на правильное количество символов.
+Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
+Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
+Для добавления стилей, используй CSS-классы valid и invalid.
+*/
+
+const testInputRef = document.querySelector('#validation-input');
+// console.dir(testInputRef);
+
+testInputRef.addEventListener('focus', onInputFocus);
+testInputRef.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event) {
+  const validLengthRef = Number(testInputRef.getAttribute('data-length'));
+  const valueInputRef = event.target.value;
+  return valueInputRef.length === validLengthRef
+    ? testInputRef.classList.add('valid')
+    : testInputRef.classList.add('invalid');
+}
+function onInputFocus() {
+  testInputRef.classList.remove('valid', 'invalid');
+}
